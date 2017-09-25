76043297abb0f6b476d19a506cc30c030a14a9e1
Diego Vega <Chino@MBP-de-Chino.hsd1.il.comcast.net>
2017-09-24 22:09:25 -0500

    [dvega3] Walls are solid. Object moves with force instead of transform (accelerates/slows down). Scenery (Diego's part) DONE.

--- ranibow-wars-unity/Assets/Scripts/CharacterController.cs
+++ ranibow-wars-unity/Assets/Scripts/CharacterController.cs
@@ -9,23 +9,23 @@ public class CharacterController : MonoBehaviour {
     float cameraRotX = 0f;
     public float cameraPitchMax = 70f;
 
+    private Rigidbody rbody;
+
  	// Use this for initialization
  	void Start () 
  	{
-  		moveSpeed = 6f;
+  		moveSpeed = 400f;
+  		rbody = GetComponent<Rigidbody>();
  	}
  
  	// Update is called once per frame
  	void Update () 
  	{
-  		transform.Translate(-1*moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,-1*moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
- 		
+ 		rbody.AddForce(-1*moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,-1*moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+  		//transform.Translate(-1*moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,-1*moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 	
  		cameraRotX -= Input.GetAxis ("Mouse X") * -5f;
-   
         cameraRotX = Mathf.Clamp (cameraRotX, -cameraPitchMax, cameraPitchMax);
- 
         Quaternion rot = Quaternion.Euler (0, cameraRotX, 0);
- 
         transform.localRotation = rot;
  	}
 }﻿
