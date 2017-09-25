5b4a5d58a3faef72fe84aa6f98c60b82f6bf7181
Diego Vega <Chino@MBP-de-Chino.hsd1.il.comcast.net>
2017-09-24 19:50:00 -0500

    [dvega3] Added character and basic movement functions.

---
+++ ranibow-wars-unity/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float moveSpeed;
+
+ 	// Use this for initialization
+ 	void Start () 
+ 	{
+  		moveSpeed = 6f;
+ 	}
+ 
+ 	// Update is called once per frame
+ 	void Update () 
+ 	{
+  		transform.Translate(-1*moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,-1*moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
+ 	}
+}﻿
