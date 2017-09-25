763060f6882079d38a2dd49901d1e7d1277726c4
Diego Vega <Chino@MBP-de-Chino.hsd1.il.comcast.net>
2017-09-24 20:08:57 -0500

    [dvega3] Added character, camera movement,  and basic movement functions.

--- ranibow-wars-unity/Assets/Scripts/CharacterController.cs
+++ ranibow-wars-unity/Assets/Scripts/CharacterController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
 	public float moveSpeed;
+	public GameObject camera;
+    float cameraRotX = 0f;
+    public float cameraPitchMax = 70f;
 
  	// Use this for initialization
  	void Start () 
@@ -16,5 +19,13 @@ public class CharacterController : MonoBehaviour {
  	void Update () 
  	{
   		transform.Translate(-1*moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,-1*moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
+ 		
+ 		cameraRotX -= Input.GetAxis ("Mouse X") * -5f;
+   
+        cameraRotX = Mathf.Clamp (cameraRotX, -cameraPitchMax, cameraPitchMax);
+ 
+        Quaternion rot = Quaternion.Euler (0, cameraRotX, 0);
+ 
+        transform.localRotation = rot;
  	}
 }﻿
